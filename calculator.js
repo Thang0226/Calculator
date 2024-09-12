49ac2b8360474ac19d427fd6cd861e55f0bc1499
Thang0226 <thang.nd0226@gmail.com>
2024-09-12 14:31:49 +0700

    add new files

---
+++ calculator.js
@@ -0,0 +1,25 @@
+function add(){
+    let num1 = + document.getElementById("num1").value
+    let num2 = + document.getElementById("num2").value
+    let sum = num1 + num2
+    document.getElementById("result").innerHTML = sum
+}
+function subtract(){
+    let num1 = + document.getElementById("num1").value
+    let num2 = + document.getElementById("num2").value
+    let num = num1 - num2
+    document.getElementById("result").innerHTML = num
+}
+function multiply(){
+    let num1 = + document.getElementById("num1").value
+    let num2 = + document.getElementById("num2").value
+    let num = num1 * num2
+    document.getElementById("result").innerHTML = num
+}
+function divide(){
+    let num1 = + document.getElementById("num1").value
+    let num2 = + document.getElementById("num2").value
+    let num = num1 / num2
+    num = Math.round(num*100)/100
+    document.getElementById("result").innerHTML = num
+}
